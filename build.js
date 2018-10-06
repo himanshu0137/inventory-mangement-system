e97f8972e841d5f0f9db69c6ce3ecf705e540eed
himanshu0137 <himanshu.bansal@quovantis.com>
2018-10-06 10:41:29 +0530

    added electron packager build script

---
+++ build.js
@@ -0,0 +1,20 @@
+const packager = require("electron-packager");
+packager({
+    dir: ".",
+    ignore: [
+        /\.gitignore/,
+        /ims-app/,
+        /\bdata\b/,
+        /print\ sample/,
+        /binaries/,
+        /\.vscode/,
+        /package-lock\.json/,
+        /build.js/
+    ],
+    arch: "ia32",
+    platform: "win32",
+    executableName: "GCR_Dyeing",
+    out: "./binaries",
+    name: "GCR Dyeing",
+    overwrite: true
+}).then(appPaths => console.log(appPaths)).catch(error => console.log(error));
